531f2c59e27e11a28d521ae95c49525ae51c316b
Carson Jones <carsonjones@Carsons-MacBook-Pro.local>
2016-07-22 00:21:17 -0500

    Updated to most of new layout and added parallax

--- main/js/homepage.js
+++ main/js/homepage.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
   var $desktop = $(".desktop");
 
 
+
   function checkWidth() {
     var windowsize = $window.width();
 
@@ -22,18 +23,6 @@ $(document).ready(function() {
   // Bind event listener
   $(window).resize(checkWidth);
 
-  // Closes the sidebar menu
-  $("#menu-close").click(function(e) {
-    e.preventDefault();
-    $("#sidebar-wrapper").toggleClass("active");
-  });
-
-  // Opens the sidebar menu
-  $("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#sidebar-wrapper").toggleClass("active");
-  });
-
 
   //Scrolls to the selected menu item on the page
   $(function() {
@@ -50,6 +39,51 @@ $(document).ready(function() {
       }
     });
   });
+
+  // parallax
+  (function($) {
+
+    $.fn.parallax = function(options) {
+
+      var windowHeight = $(window).height();
+
+      // Establish default settings
+      var settings = $.extend({
+        speed        : 0.15
+      }, options);
+
+      // Iterate over each object in collection
+      return this.each( function() {
+
+        // Save a reference to the element
+        var $this = $(this);
+
+        // Set up Scroll Handler
+        $(document).scroll(function(){
+
+          var scrollTop = $(window).scrollTop();
+          var offset = $this.offset().top;
+          var height = $this.outerHeight();
+
+          // Check if above or below viewport
+          if (offset + height <= scrollTop || offset >= scrollTop + windowHeight) {
+            return;
+          }
+
+          var yBgPosition = Math.round((offset - scrollTop) * settings.speed);
+
+          // Apply the Y Background Position to Set the Parallax Effect
+          $this.css('background-position', 'center ' + yBgPosition + 'px');
+
+        });
+      });
+    }
+  }(jQuery));
+
+  $('.jumbo, .roots-in-auburn').parallax({
+    speed :	0.15
+  });
+
 
 
 
@@ -77,121 +111,7 @@ $(document).ready(function() {
     $googleInput.css('left', (($($googleContent).width() - $($googleInput).width()) / 2) - 2);
   }
 
-  // $.scrollify({
-  //   section : ".section",
-  //   easing: "easeOutExpo",
-  //   setHeights: true,
-  //   scrollSpeed: 1100,
-  //   scrollBars: false,
-  //   after: function(i, sections) {
-  //     $id = $(sections[i]).attr("id");
-  //
-  //     switch ($id) {
-  //       case "bring-to-the-future":
-  //       //                            // Grab the current date
-  //       //                            var currentDate = new Date();
-  //       //
-  //       //                            // Set some date in the future. In this case, it's always Jan 1
-  //       //                            var futureDate  = new Date(currentDate.getFullYear() + 22, 0, 1);
-  //       //
-  //       //                            // Calculate the difference in seconds between the future and current date
-  //       //                            var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
-  //       //
-  //       //                            // Instantiate a coutdown FlipClock
-  //       //                            clock = $('.countdown').FlipClock({
-  //       //                                clockFace: 'YearlyCounter'
-  //       //                            });
-  //
-  //
-  //       var date = new Date("1990-01-05");
-  //       var today = new Date.today();
-  //       var addYearsDate = new Date("2000-01-05");
-  //       var addMonthsDate = new Date.today().addMonths(-1);
-  //       var speed = 200;
-  //       timer();
-  //       function timer() {
-  //         if (date < addYearsDate) {
-  //           date.addYears(1);
-  //
-  //           //Do code for showing the number of seconds here
-  //           $(".years").text(date.getRealYear());
-  //           $(".months").text(date.getMonthName('en'));
-  //           $(".days").text(date.getDate() + ",");
-  //
-  //           if (date >= today) {
-  //             return;
-  //           }
-  //
-  //
-  //           speed = speed / 6 * 5;
-  //           setTimeout(timer, speed);
-  //
-  //         }
-  //         else if (date < addMonthsDate) {
-  //           date.addMonths(1);
-  //
-  //           //Do code for showing the number of seconds here
-  //           $(".years").text(date.getRealYear());
-  //           $(".months").text(date.getMonthName('en'));
-  //           $(".days").text(date.getDate() + ",");
-  //
-  //           if (date >= today) {
-  //             return;
-  //           }
-  //
-  //           speed = speed / 6 * 5;
-  //           setTimeout(timer, speed);
-  //         }
-  //         else {
-  //
-  //           date.addDays(1);
-  //
-  //           //Do code for showing the number of seconds here
-  //           $(".years").text(date.getRealYear());
-  //           $(".months").text(date.getMonthName('en'));
-  //           $(".days").text(date.getDate() + ",");
-  //
-  //           if (date >= today) {
-  //
-  //             return;
-  //           }
-  //
-  //           speed = 10 + speed;
-  //           setTimeout(timer, speed);
-  //         }
-  //       }
-  //
-  //
-  //       break;
-  //       case "first-impressions":
-  //
-  //       videojs("impression-vid", {
-  //         fluid: true,
-  //         aspectRatio : "16:9",
-  //         "controls" : false,
-  //         "autoplay" : true,
-  //         "preload" : "auto",
-  //         "poster" : "",
-  //         "techOrder": ["youtube"],
-  //         "sources": [{
-  //           "type": "video/youtube",
-  //           "src": "https://www.youtube.com/watch?v=8rYw0z-K2Nw"
-  //         }]
-  //       });
-  //
-  //       var video = videojs('impression-vid').ready(function(){
-  //         var player = this;
-  //
-  //         player.on('ended', function() {
-  //
-  //         });
-  //       });
-  //       break;
-  //     }
-  //   }
-  // });
-  //
-  // $.scrollify.disable();
+
 
   Date.prototype.getRealYear = function() {
     if(this.getFullYear)
--- main/pages/homepage.php
+++ main/pages/homepage.php
@@ -21,298 +21,100 @@
     <link rel="stylesheet" href="../../bower_components/video.js/dist/video-js.min.css">
     <link rel="stylesheet" href="../css/homepage.css" type="text/css">
     <!--endbuild-->
-
-    <script src="../../bower_components/video.js/dist/video.min.js"></script>
+    <script src="../../bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="../../bower_components/parallax.js/parallax.js"></script>
   </head>
   <body>
-    <nav id="sidebar-wrapper">
-      <ul class="sidebar-nav">
-        <a id="menu-close" href="#" class="btn btn-light btn-lg pull-right toggle"><i class="fa fa-times"></i></a>
-        <li class="sidebar-brand">
-          <a href="#top"  onclick = $("#menu-close").click();>Mad Men Software</a>          
-        </li>
-        <li>
-          <a href="#top" onclick = $("#menu-close").click();>Home</a>
-        </li>
-        <li>
-          <a href="#about" onclick = $("#menu-close").click();>About</a>
-        </li>
-        <li>
-          <a href="#services" onclick = $("#menu-close").click();>Services</a>
-        </li>
-        <li>
-          <a href="#portfolio" onclick = $("#menu-close").click();>Portfolio</a>
-        </li>
-        <li>
-          <a href="#contact" onclick = $("#menu-close").click();>Contact</a>
-        </li>
-      </ul>
-    </nav>
+    <section class="bg-1 text-center jumbo">
+        <div class="container-fluid">
+          <div class="row top">
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 col-xxs-3"></div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 col-xxs-6 logo-container">
+              <img class="img-responsive logo animated flipInX" src="../img/MadMenOfficialLogo%20w:o%20Tagline.png">
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 col-xxs-3">
+            </div>
+            </div>
+            <div class="row middle">
+              <div class="col-lg-12 middle-col">
+                  <h1 class="animated bounceInLeft text-center">Bring your Ideas to Life</h1>
+              </div>
+            </div>
+            <div class="row bottom">
+              <div class="col-lg-12 bottom-col">
+                <div class="scroll-down-container">
+                  <a href="" class="scroll-down-btn">
+                    <span class="fa-stack fa-2x">
+                      <i class="fa fa-circle fa-stack-2x icon-background"></i>
+                      <i class="fa fa-angle-down fa-stack-1x" aria-hidden="true"></i>
+                    </span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </section>
 
-    <div class="jumbotron section" id="top">
+    <section class="touch-of-madness">
       <div class="container-fluid">
-        <div class="row top">
-          <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 col-xxs-3"></div>
-          <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 col-xxs-6 logo-container">
-            <img class="img-responsive logo animated flipInX" src="../img/MadMenOfficialLogo%20w:o%20Tagline.png">
-          </div>
-          <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 col-xxs-3">
-            <div class="menu-toggle-container center-icon">
-              <a id="menu-toggle" href="#" class="btn btn-dark btn-lg toggle animated fadeIn">
-                <i class="fa fa-bars fa-fw"></i>
-              </a>
-            </div>
-          </div>
-          </div>
-          <div class="row middle">
-            <div class="col-lg-12 middle-col">
-              <div class="jumbotron-header pull-right">
-                <h1 class="animated bounceInLeft">Don't Get Stuck</h1>
-                <h1 class="animated bounceInLeft">in the Past</h1>
-                <p class="subtitle">Stop losing customers because your online presence is lacking. Your website is often the first thing potential customers see, they’re likely to dismiss you entirely should they believe your website doesn’t reflect the kind of experience your business.</p>
-                <button class="btn btn-primary call-to-action-btn">We Can Help</button>
-              </div>
-
-            </div>
-          </div>
-          <div class="row bottom">
-            <div class="col-lg-12 bottom-col">
-              <div class="scroll-down-container">
-                <a href="#benefits-row" class="scroll-down-btn">
-                  <span class="fa-stack fa-2x">
-                    <i class="fa fa-circle fa-stack-2x icon-background"></i>
-                    <i class="fa fa-angle-down fa-stack-1x" aria-hidden="true"></i>
-                  </span>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="container-fluid main-container">
-      <div class="row" id="benefits-row">
-
-        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-6 benefits-col" >
-          <i class="fa fa-hand-o-up" id="new-business" aria-hidden="true"></i>
-          <h3>Attract New Business</h3>
-        </div>
-        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-6 benefits-col">
-          <i class="fa fa-google" id="credibility" aria-hidden="true"></i>
-          <h3>Enhance Credibility</h3>
-        </div>
-
-        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-6 benefits-col" >
-          <i class="fa fa-smile-o" id="engage" aria-hidden="true"></i>
-          <h3>Engage with Customers</h3>
-        </div>
-        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-6 benefits-col" >
-          <i class="fa fa-paper-plane-o" id="get-message-out" aria-hidden="true"></i>
-          <h3>Get your Message Out</h3>
-        </div>
-      </div>
-      <div class="row difference-row">
-        <div class="col-lg-6">
-          <h3>Mad Men Software is Different</h3>
-          <p>We Aren't your Average Software Contractor</p>
-          <ul>
-            <li>Personal Touch</li>
-            <li>Hard-working</li>
-            <li>Creative</li>
-          </ul>
-          <a class="btn btn-primary"></a>
-        </div>
-      </div>
-      <div class='row'>
-        <div class="col-lg-6 pull-right">
-          <h3>Custom Work</h3>
-          <p>We'll make you w</p>
-        </div>
-      </div>
-
-      <div class="row section" id="who-we-are">
-        <div class="col-lg-12">
-          <div class="row">
-            <div class="col-lg-3">
-              <h3>“No great mind has ever existed without a touch of madness.” - Aristotle</h3>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-lg-12">
-              <h3>We are designers, developers, and creative thinkers. That's why we call ourselves Mad Men</h3>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-lg-12">
-              <h3>Mad Men can Make you Anything</h3>
-              <a class="btn btn-primary">Learn More About Us</a>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="row section" id="first-impressions">
-        <div class="col-lg-12 content">
-          <h1>First Impressions Matter</h1>
-          <div class="video-wrapper">
-
-
-
-            <div class="video">
-              <video id="impression-vid" class="video-js vjs-default-skin" controls muted preload="auto" width="560" height="315" style="width: 100%; height: 90%; padding-top: 0;">
-                <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that
-                  <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
-                </p>
-              </video>
-            </div>
-
-          </div>
-
-
-        </div>
-      </div>
-
-      <div class="row section" id="vital-presence">
-        <div class="col-lg-12">
-          <h1>Online Presence is Vital for your Business</h1>
-        </div>
-      </div>
-
-      <div class="row section" id="expand-your-tech">
-        <div class="col-lg-12">
-          <h1>Let Us Expand your Business through Technology</h1>
-        </div>
-      </div>
-
-      <div class="row section" id="impress-customers">
-        <div class="col-lg-12">
-          <h1>Impress potential customers.</h1>
-        </div>
-      </div>
-
-      <div class="row section" id="keep-engaged">
-        <div class="col-lg-12">
-          <h1>Keep your Customers Engaged</h1>
-        </div>
-      </div>
-
-      <div class="row section" id="get-message-across">
-        <div class="col-lg-12">
-          <h1>Get your Message Across</h1>
-        </div>
-      </div>
-
-
-      <div class="row section" id="services">
-        <div class="col-lg-12">
-          <div class="row content">
-            <div class="col-lg-6">
-              <h1>Our Services</h1>
-              <hr>
-              <p class="lead">We Offer a Wide Variety of Services for All of your Business Needs</p>
-              <p>Let us create a stunning website for your business! Mad Men Software is a professional web design company that specializes in creating gorgeous websites for small businesses.</p>
-              <button class="btn btn-primary btn-lg">Learn More</button>
-            </div>
-            <div class="col-lg-6">
-              <img class="img-responsive" src="../img/all-devices.png">
-            </div>
-          </div>
-          <div class="row graphic">
-            <div class="col-lg-3 col-md-3 col-xs-6 services">
-              <a href=""><i class="fa fa-5x fa-laptop"></i></a>
-              <h2>Web Design <br> and Development</h2>
-            </div>
-            <div class="col-lg-3 col-md-3 col-xs-6 services">
-              <a href=""><i class="fa fa-5x fa-mobile-phone"></i></a>
-              <h2>Mobile Application <br> Development</h2>
-            </div>
-            <div class="col-lg-3 col-md-3 col-xs-6 services">
-              <a href=""><i class="fa fa-5x fa-cloud"></i></a>
-              <h2>Small Business <br>Technology Infrastructure</h2>
-            </div>
-            <div class="col-lg-3 col-md-3 col-xs-6 services">
-              <a href=""><i class="fa fa-5x fa-life-ring"></i></a>
-              <h2>Web Hosting</h2>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="row section" id="packages">
-        <div class="col-lg-12">
-          <h1>Packages</h1>
-        </div>
-      </div>
-
-      <div class="row section" id="contact">
-        <div class="col-lg-12">
-          <h1>Contact Us</h1>
-        </div>
-      </div>
-
-      <div class="row section" id="section-four">
-        <div class="col-lg-6">
-          <h1>Fast and Beautiful Design</h1>
-          <hr>
-          <p class="lead">Lorem Ipsum Lorem Ipsum Lorem Ipsum</p>
-          <p>Lorem Ipsum Lorem Ipsum Lorem IpsumLorem Ipsum Lorem Ipsum Lorem IpsumLorem Ipsum Lorem Ipsum Lorem IpsumLorem Ipsum Lorem Ipsum Lorem Ipsum</p>
-          <button class="btn btn-primary btn-lg">Get a Quote</button>
-        </div>
-        <div class="col-lg-6">
-          <img class="img-responsive" src="../img/all-devices.png">
-        </div>
-      </div>
-      <div class="row section" id="section-five">
-        <div class="col-lg-10 col-lg-offset-1 text-center">
-          <h1>Portfolio</h1>
-          <p>         </p>
-          <p style="font-style:italic">"Helping Local to Long Distance Every Step of the Way"</p>
-          <h3> On The Web.</h3>
-          <hr class="small">
-          <div class="row">
-            <div class="col-md-6 items">
-              <div class="portfolio-item">
-                <a href="#">
-                  <a target="_blank" href="http://www.beangourmetmarketplace.com/">
-                    <img class="img-portfolio img-responsive" src="../img/bean.png">
-                  </a>
-                </div>
-              </div>
-              <div class="col-md-6 items">
-                <div class="portfolio-item">
-                  <a href="#">
-                    <img class="img-portfolio img-responsive" src="../img/compost.png">
-                  </a>
-                </div>
-              </div>
-            </div>
-            <!-- /.row (nested) -->
-            <p>                      </p>
-          </div>
-          <!-- /.col-lg-10 -->
-        </div>
-        <div class="row section" id="section-six">
-          <h1>FAQ</h1>
-          <p>Dropdown Boxes with Answers to Questions</p>
-        </div>
-        <div class="row section" id="section-seven">
-          <h1>Package Section</h1>
-          <p>Graphic under with web, mobile, Mad Pack</p>
-        </div>
-        <div class="row section" id="section-eight">
-          <h1>Contact Us Form</h1>
-          <p>Graphic under with web, mobile, Mad Pack</p>
-        </div>
-        <div class="row section" id="section-nine">
+        <div class="row" id="touch-of-madness">
           <div class="col-lg-12">
-            <h1>Why do you need a new website?</h1>
-            <p>Having a website for your business these days is pretty much a must. <strong>Because most people Google before they shop or dine somewhere, your website is your customer's first impression of your business. Beginning with your website, consumers are passing judgement and making decisions about whether or not they will even visit your store, restaurant or office. They’re likely to dismiss you entirely, as well, should they believe your website doesn’t reflect the kind of experience your business – or a business like yours – should offer. If you don’t have a website, you’re missing out on opportunities for customers to identify who you are and if they want to spend money with you. While no website equals missed opportunities, a bad website can actually be worse since it literally makes your business look bad. Having a professional, polished looking website that functions easily and offers customers easy navigation, strong photo images, professional quality content and an overall experience that engages them enough for them to want to do business with you is key.</strong> Let us do the work for you. We will create a beautiful, professional looking site for your business that's a joy for visitors to use which will drive more customers to your business and keep them coming back.</p>
-            <button class="btn btn-primary btn-lg">Get a Quote</button>
+            <p class="text-center">with a</p>
+            <h1 class="text-center">Touch of Madness</h1>
+            <img class="img-responsive" src="../img/responsive-screens.png"/>
+            <h1 class="text-center">Anything is possible.</h1>
           </div>
         </div>
       </div> <!-- end main-container -->
-    </div>
+    </section>
+
+    <section class="roots-in-auburn">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-lg-12">
+              <h1 class="text-center">with Roots in Auburn</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="benefits">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-lg-6 pull-left personal-touch benefits-section">
+            <h1 class="text-center">Personal Touch</h1>
+            <p>
+              Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum.
+              Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum.
+              Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum.
+            </p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-6 pull-right easy benefits-section">
+            <h1 class="text-center">Big Problems Solved Simply</h1>
+            <p>
+              Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum.
+              Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum.
+              Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum.
+            </p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-6 pull-left custom benefits-section">
+            <h1 class="text-center">Custom</h1>
+            <p>
+              Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum.
+              Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum.
+              Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
     <footer class="section">
       <div class="container-fluid">
         <div class="row logo-row">
@@ -349,7 +151,6 @@
     </footer><!-- end footer -->
 
     <!--build:js /js/main.min.js -->
-    <script src="../../bower_components/jquery/dist/jquery.min.js"></script>
     <script src="../../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="../../bower_components/jquery-ui/jquery-ui.min.js"></script>
     <script src="../../bower_components/typed.js/dist/typed.min.js"></script>
